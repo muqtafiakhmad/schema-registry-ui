69952840a03f19fc4a35af3e5c889baad2d56dfe
Ubuntu <ubuntu@data-datahub-schemareg-01.prod.data.tvlk.cloud>
2016-11-30 12:10:09 +0000

    handle null password

--- src/schema-registry/home/home.controller.js
+++ src/schema-registry/home/home.controller.js
@@ -1,6 +1,6 @@
 angularAPP.controller('HomeCtrl', function ($log, SchemaRegistryFactory, toastFactory, UtilsFactory) {
   var password = prompt("Please enter your password", "");
-  while(UtilsFactory.hashSha256(password) != ADMIN_PASSWORD_HASH){
+  while(password == null || UtilsFactory.hashSha256(password) != ADMIN_PASSWORD_HASH){
     password = prompt("Please enter your password", "");
   }
   $log.info("Starting schema-registry controller - home");

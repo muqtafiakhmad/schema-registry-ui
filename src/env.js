ab3a25a60f60630c8e83ad24c406d28448342f8c
Muqtafi Akhmad <muqtafi@traveloka.com>
2016-11-30 12:37:48 +0700

    add simple password auth on home controller

--- src/env.js
+++ src/env.js
@@ -4,3 +4,4 @@ var SCHEMA_REGISTRY = "http://localhost:8081"; // https://schema-registry.demo.l
 // Avro4S backend url. Allows converting Avro -> Scala Case classes
 var AVRO4S = "https://platform.landoop.com/avro4s/avro4s";
 
+var ADMIN_PASSWORD_HASH = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"; // sha 256 of "admin"
--- src/schema-registry/home/home.controller.js
+++ src/schema-registry/home/home.controller.js
@@ -1,4 +1,9 @@
 angularAPP.controller('HomeCtrl', function ($log, SchemaRegistryFactory, toastFactory) {
+  sha256 = require('js-sha256').sha256;
+  var password = prompt("Please enter your password", "");
+  while(sha256(password) != ADMIN_PASSWORD_HASH){
+    password = prompt("Please enter your password", "");
+  }
   $log.info("Starting schema-registry controller - home");
   toastFactory.hideToast();
 });
